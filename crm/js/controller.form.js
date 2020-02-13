93561906fe9636ce3baaeebccf777ee5d58ea730
Arozhdan <halala30@gmail.com>
2020-02-13 16:20:30 +0100

    crm

--- crm/js/controller.form.js
+++ crm/js/controller.form.js
@@ -5,8 +5,8 @@
   api.start = function () {
     document.querySelector('#createOrder')
       .addEventListener('click', function () {
-        let newOrderData= collectData()
-        if(newOrderData.name,newOrderData.email,newOrderData.phone)    Model.createOrder(newOrderData)
+        let newOrderData = collectData()
+        if (newOrderData.name, newOrderData.email, newOrderData.phone) Model.createOrder(newOrderData)
         alert(newOrderData.name)
       })
   }
@@ -16,14 +16,14 @@
     const newOrder = {
       status: 'new'
     }
-    newOrder.date=new Date
-    
-    newOrder.name=document.querySelector('.input-name').value
-    newOrder.email=document.querySelector('.input-email').value
-    newOrder.phone=document.querySelector('.input-phone').value
-    newOrder.product=document.querySelector('.input-product').value
-    
-    
+    newOrder.date = new Date
+
+    newOrder.name = document.querySelector('.input-name').value
+    newOrder.email = document.querySelector('.input-email').value
+    newOrder.phone = document.querySelector('.input-phone').value
+    newOrder.product = document.querySelector('.input-product').value
+
+
     return newOrder
   }
 })();
