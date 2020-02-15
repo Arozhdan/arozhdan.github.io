a1663b2d7e5ff7ea0387445d7766fbb8eb95eda9
Arozhdan <halala30@gmail.com>
2020-02-15 14:36:17 +0100

    eshop

--- eshop/js/View-main.js
+++ eshop/js/View-main.js
@@ -1,62 +1,64 @@
-;(function () {
-    "use strict"
+;
+(function () {
+  "use strict"
+  const api = {}
 
-    const api = {}
+  window.View = api
+  api.getFlatCard = getFlatCard
+  api.getShowcase = function getShowcase(flats) {
+    const rootElement = document.createElement('div')
+    rootElement.classList.add("cards-wrapper")
+    const containerElement = document.createElement('div')
+    containerElement.classList.add("container", "p-0")
+    rootElement.append(containerElement)
+    const rowElement = document.createElement("row")
+    rowElement.classList.add("row")
+    containerElement.append(rowElement)
+    rowElement.append(...flats.map(getFlatCard))
+    return rootElement
+  }
+  api.getTable = function getTable(flats) {
+    const divElement= document.createElement('div')
+    divElement.innerHTML=tableTemplate
+    divElement.querySelector('[data-items]').append(...flats.map(getTableItem))
+    return divElement.firstElementChild
+  }
+  function getTableItem(flat) {
+    const divElement= document.createElement('div')
+    divElement.innerHTML=tableItemTemplate
+    .replace(/%ID%/g, flat.id)
+    .replace(/%LABEL%/, flat.label)
+    .replace(/%PRICE%/, flat.price)
+    .replace(/%SQUARE%/, flat.square)
+    .replace(/%NOMENCLATURE%/, flat.nomenclature)
+    .replace(/%FLOOR%/, flat.floor)
+    .replace(/%ROOMS%/, flat.rooms)
+    .replace(/%PRICE_PER_SQUARE%/g, parseInt(flat.price/flat.square))
 
-    api.getShowcase = function getShowcase (flats) {
-        const rootElement = document.createElement('div')
-        rootElement.classList.add("cards-wrapper")
 
-        const containerElement = document.createElement('div')
-        containerElement.classList.add("container", "p-0")
-        rootElement.append(containerElement)
+    .replace(/%PANEL_LIKE_ACTIVE%/g, flat.favorite ? "panel__favourite-btn--active" : "")
+    return divElement.firstElementChild
+    
+  }
 
-        const rowElement = document.createElement("row")
-        rowElement.classList.add("row")
-        containerElement.append(rowElement)
+  function getFlatCard(flat) {
+    const divElement = document.createElement('div')
+    divElement.innerHTML = cardTemplate
+      .replace(/%ID%/g, flat.id)
+      .replace(/%LABEL%/g, flat.label)
+      .replace(/%ROOMS%/g, flat.rooms)
+      .replace(/%PRICE%/g, flat.price)
+      .replace(/%SQUARE%/g, flat.square)
+      .replace(/%PRICE_PER_SQUARE%/g, parseInt(flat.price/flat.square))
+      .replace(/%NOMENCLATURE%/g, flat.nomenclature)
+      .replace(/%FLOOR%/g, flat.floor)
+      .replace(/%COMON_FLOOR%/g, flat.commonFloor)
+      
+      .replace(/%CARD_LIKE_ACTIVE%/g, flat.favorite ? "card__like--active" : "")
 
-        rowElement.append(...flats.map(getFlatCard))
-
-        return rootElement
-    }
-
-    api.getTable = function getTable (flats) {
-        const divElement = document.createElement('div')
-
-        divElement.innerHTML = tableTemplate
-        divElement.querySelector('[data-items]').append(...flats.map(getTableItem))
-
-        return divElement.firstElementChild
-    }
-
-    window.View = api
-
-    function getFlatCard (flat) {
-        const divElement = document.createElement('div')
-
-        divElement.innerHTML = cardTemplate
-            .replace(/%ID%/g, flat.id)
-            .replace(/%LABEL%/g, flat.label)
-            .replace(/%ROOMS%/g, flat.rooms)
-            .replace(/%PRICE%/g, flat.price)
-            .replace(/%SQUARE%/g, flat.square)
-            .replace(/%CARD_LIKE_ACTIVE%/g, flat.favorite ? "card__like--active" : "")
-
-        return divElement.firstElementChild
-    }
-
-    function getTableItem (flat) {
-        const divElement = document.createElement('div')
-
-        divElement.innerHTML = tableItemTemplate
-            .replace(/%LABEL%/g, flat.label)
-            .replace(/%PRICE%/g, flat.price)
-            .replace(/%SQUARE%/g, flat.square)
-
-        return divElement.firstElementChild
-    }
-
-    const cardTemplate = `
+    return divElement.firstElementChild
+  }
+  const cardTemplate = `
 <div class="col-md-4">
     <div class="card">
         <div class="card__header">
@@ -71,7 +73,7 @@
         <div class="card__desc">
             <div class="card__price">
                 <div class="card__price-total">%PRICE% ₽</div>
-                <div class="card__price-per-meter">%PRICE_PRE_SQUARE% ₽/м2</div>
+                <div class="card__price-per-meter">%PRICE_PER_SQUARE% ₽/м2</div>
             </div>
 
             <!-- card__params params -->
@@ -94,9 +96,8 @@
         </div>
     </div>
 </div>`
-
-    const tableTemplate = `
-<div class="panels-wrapper">
+  const tableTemplate = `
+  <div class="panels-wrapper">
     <div class="container p-0">
         <div class="panels-filter">
             <div class="panels-filter__element" style="width: 120px;">
@@ -134,29 +135,28 @@
             <div class="panels-filter__element" style="width: 100px;">
                 <div class="panels-filter__name">Избранное</div>
             </div>
-        </div>
+          </div>
 
-        <div data-items></div>
-    </div>
-</div>`
-
-    const tableItemTemplate = `
-<div class="panel">
-    <div class="panel__artikul">ГЕН-112.4.2-56</div>
-    <div class="panel__name">
-        <a href="#">%LABEL%</a>
-    </div>
-    <div class="panel__block">15</div>
-    <div class="panel__floor">4</div>
-    <div class="panel__rooms">1</div>
-    <div class="panel__sq">%SQUARE% м2</div>
-    <div class="panel__price-per-m">64 000 ₽</div>
-    <div class="panel__price">%PRICE% ₽</div>
-    <div class="panel__seller">Застройщик</div>
-    <div class="panel__favourite">
-        <button class="panel__favourite-btn panel__favourite-btn--active">
-            <i class="fas fa-heart"></i>
-        </button>
-    </div>
-</div>`
+          <div data-items></div>
+      </div>
+  </div>`
+  const tableItemTemplate = `
+  <div class="panel">
+      <div class="panel__artikul">%NOMENCLATURE%</div>
+      <div class="panel__name">
+          <a href="#">%LABEL%</a>
+      </div>
+      <div class="panel__block">15</div>
+      <div class="panel__floor">%FLOOR%</div>
+      <div class="panel__rooms">%ROOMS%</div>
+      <div class="panel__sq">%SQUARE% м2</div>
+      <div class="panel__price-per-m">%PRICE_PER_SQUARE% ₽</div>
+      <div class="panel__price">%PRICE% ₽</div>
+      <div class="panel__seller">Застройщик</div>
+      <div class="panel__favourite">
+          <button class="panel__favourite-btn %PANEL_LIKE_ACTIVE%" data-panel-id="%ID%">
+              <i class="fas fa-heart"></i>
+          </button>
+      </div>
+  </div>`
 })();
